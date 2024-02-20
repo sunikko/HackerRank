86b344fcd3afce9a9540f5b002b5952fec9b60b3
sunikko <sunikko77@gmail.com>
2024-02-20 00:33:11 +0000

    Update re_start_re_end.py
    RegEx basic: calculate with Re.start()  and Re.end()

--- Python (Intermediate)/re_start_re_end.py
+++ Python (Intermediate)/re_start_re_end.py
@@ -1,17 +1,20 @@
 import re
 
-input_s = 'aaadaa'
-input_k = 'aa'
+input_s = input()
+input_k = input()
 
 s = 0
 e = 0
-
-for i in range(len(input_s)):
-    m = re.search(r'({})'.format(input_k), input_s[s+i:])
+idx = 0
+while idx < len(input_s):
+    m = re.search(r'({})'.format(input_k), input_s[s:])
     if m is not None:
-        s = m.start() + i
-        e = m.end() + (i - 1)
+        # print('start:',m.start(), ', end:', m.end())
+        s = m.start() + idx
+        e = m.end() + (idx - 1)
         print((s,e))
+    s += 1
+    idx = s
         
 if e == 0:
-    print(-1, -1)
+    print((-1, -1))
