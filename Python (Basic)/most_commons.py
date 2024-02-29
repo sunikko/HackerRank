71ac9e28b18424f9aeeeb7d261f702fa35b1eaf8
sunikko <sunikko77@gmail.com>
2024-02-29 12:27:17 +0000

    Update most_commons.py
    [python print lists](https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/)

--- Python (Basic)/most_commons.py
+++ Python (Basic)/most_commons.py
@@ -23,10 +23,22 @@ if __name__ == '__main__':
     # Python can sort by two things by returning a tuple for the key in the sorted method
     #  the negative sign is required on the numeric type to reverse its sorted order.
     # chr_dict = sorted(chr_dict.items(), key=lambda item: (item[-1], item[0])) ?? 
+    # == 1 ==
     # chr_list = sorted(chr_dict.items(), key=lambda item: item[0])
     # chr_list = sorted(chr_list, key=lambda item: item[1], reverse=True)
+    # == 2 ==
     chr_list = sorted(chr_dict.items(), key=lambda item: (-item[1], item[0]))
+
+    # == 1 ==
+    # for item in chr_list[:3]:
+    #     print("{} {}".format(item[0], item[1]))
+    # == 2 ==
+    # for chr, count in chr_list[:3]:
+    #         print(chr, count)
+    # == 3 ==
+    # * symbol is used to print the list elements in a single line with space
+    for i in chr_list[:3]:
+        print(*i)
+
     
-    for item in chr_list[:3]:
-        print("{} {}".format(item[0], item[1]))
     
