335057f4b51a7a175f12e0184ad8158183f4344a
sunikko <sunikko77@gmail.com>
2024-03-14 15:36:24 +0000

    Create python_sort_sort.py

---
+++ Python (Basic)/python_sort_sort.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+
+    k = int(input())
+    
+    arr = sorted(arr, key=lambda x: x[k])
+    for item in arr:
+        print(*item)
